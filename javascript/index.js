f6cdc5080698ca3e8ddaf97c8fbf901f21aa9e17
Landen Nguyen <landenttvnguyen@gmail.com>
2023-08-21 13:23:22 -0500

    Refactor with switch cases and reformat

--- javascript/index.js
+++ javascript/index.js
@@ -11,67 +11,70 @@ let computerScore = 0;
 // event listeners for buttons
 
 rock.addEventListener('click', () => {
-    
   let result = playRound('rock');
   game(result);
-    
 });
     
 paper.addEventListener('click', () => {
-    
   let result = playRound('paper');
   game(result);
-    
 });
     
 scissors.addEventListener('click', () => {
-    
   let result = playRound('scissors');
   game(result);
-    
 });
 
 // functions
 
 function getComputerChoice () {
-
   const options = ["rock", "paper", "scissors"];
   const random = Math.floor(Math.random() * options.length);
 
   return options[random];
-
 }
 
 function playRound(choice) {
-
   let playerSelection = choice;
   let computerSelection = getComputerChoice();
 
   console.log(`Your choice: ${playerSelection}`);
   console.log(`CPU choice: ${computerSelection}`);
 
-  if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You win! Rock beats scissors!";
-  } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You lose! Paper beats rock!";
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You win! Paper beats rock!";
-  } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You lose! Scissors beats paper!";
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You win! Scissors beats paper!";
-  } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You lose! Rock beats scissors!";
-  } else if (playerSelection == computerSelection) {
-    return "Tie. Try again.";
-  } else {
-    return "Invalid input, try again.";
+  switch (playerSelection) {
+    case 'rock':
+      switch (computerSelection) {
+        case 'rock':
+          return "Tie. Try again.";
+        case 'paper':
+          return "You lose! Paper beats rock!";
+        case 'scissors':
+          return "You win! Rock beats scissors!";
+      }
+    case 'paper':
+      switch (computerSelection) {
+        case 'rock':
+          return "You win! Paper beats rock!";
+        case 'paper':
+          return "Tie. Try again.";
+        case 'scissors':
+          return "You lose! Scissors beats paper!";
+      }
+    case 'scissors':
+      switch (computerSelection) {
+        case 'rock':
+          return "You lose! Rock beats scissors!";
+        case 'paper':
+          return "You win! Scissors beats paper!";
+        case 'scissors':
+          return "Tie. Try again.";
+      }
+    default:
+      return "Invalid input, try again.";
   }
-
 }
 
 function game(roundResult) {
-
   if (roundResult == "You win! Rock beats scissors!"
    || roundResult == "You win! Paper beats rock!" 
    || roundResult == "You win! Scissors beats paper!") {
@@ -95,5 +98,4 @@ function game(roundResult) {
   } else {
     resultDiv.textContent = roundResult;
   }
-
 }
